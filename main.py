fe9cc83c7a045b0c51cc64c9bd33d3305a269cc7
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2022-10-28 14:08:56 +0200

    4

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 from util import logcfg
 #Tarea que simula la fabricación de un juego de ruedas. Se trata de un bucle infinito en el que espera un tiempo aleatorio entre 0 y 1 segundo y envía "ruedas" a la cola entrada_fabrica
-async fabricar_ruedas():
+async def fabricar_ruedas():
     while True:
        await asyncio.sleep(random.random())
        await entrada_fabrica.put("ruedas")
@@ -21,3 +21,7 @@ async def fabricar_chasis():
 
 
 if __name__ == "__main__":
+
+
+entrada_fabrica = asyncio.Queue()
+salida_fabrica = asyncio.Queue()
