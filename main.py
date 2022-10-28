603811aaea0ee4b17768ebe70aef0a54abf7c459
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2022-10-28 14:14:07 +0200

    6

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging
 import random
-import time
 
 from util import logcfg
 #Tarea que simula la fabricación de un juego de ruedas. Se trata de un bucle infinito en el que espera un tiempo aleatorio entre 0 y 1 segundo y envía "ruedas" a la cola entrada_fabrica
@@ -20,7 +19,26 @@ async def fabricar_motor():
         await asyncio.sleep(random.random())
         await entrada_fabrica.put("motor")
 
-
+#Tarea que simula la fábrica. Es un bucle infinito que va leyendo de la cola entrada_fabrica y va almacenando en variables internas el número de juegos de ruedas, el número de chasis y el número de motores. Tras leer de la cola, puede ocurrir que pueda completar un coche (si hay al menos un chasis, un juego de ruedas y un motor). Si es así, espera un tiempo aleatorio entre 0 y 1 segundo, imprime en el log "Coche fabricado" y pone "coche" en la cola salida_fabrica. Por supuesto, la fabricación de un coche implica reducir los contadores de ruedas, chasis y motor
+async def fabrica():
+    ruedas = 0
+    chasis = 0
+    motor = 0
+    while True:
+        elemento = await entrada_fabrica.get()
+        if elemento == "ruedas":
+            ruedas += 1
+        elif elemento == "chasis":
+            chasis += 1
+        elif elemento == "motor":
+            motor += 1
+        if ruedas > 0 and chasis > 0 and motor > 0:
+            await asyncio.sleep(random.random())
+            logging.info("Coche fabricado")
+            ruedas -= 1
+            chasis -= 1
+            motor -= 1
+            await salida_fabrica.put("coche")
 
 
 if __name__ == "__main__":
